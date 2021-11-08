152fef0572df0fd86e81ada97907a774962f3230
Avard3 <iurriy.zhur@gmail.com>
2021-11-08 03:51:32 +0200

    finish code

--- cp_3/zhurybeda_fb-91_cp_3/lab_3.py
+++ cp_3/zhurybeda_fb-91_cp_3/lab_3.py
@@ -1,4 +1,4 @@
-from math import *
+from lab_math import *
 
 file = open("07.txt", 'rt')
 TEXT = file.read()
@@ -24,8 +24,74 @@ top_cypher_bi = list(sorted_freq)[:5]
 top_bi = ["ст", "но", "то", "на", "ен"]
 
 alph = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х',
-        'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+        'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'э', 'ю', 'я']
+m = len(alph)
 
 
+def bi_to_number(bigram):
+    return (alph.index(bigram[0]) * m + alph.index(bigram[1])) % m ** 2
 
 
+# print(bi_to_number('вб'))
+def decrypt(text, keys):
+    a = int(keys[0])
+    b = int(keys[1])
+    decrypt_text = []
+    for i in range(0, len(text), 2):
+        x = (re(a, m ** 2) * (bi_to_number(text[i:i + 2]) - b)) % (m ** 2)
+        decrypt_text.append(alph[x // m] + alph[x % m])
+    return ''.join(i for i in decrypt_text)
+
+
+def pair(text):
+    couple = []
+    pairs = []
+    for i in top_bi:
+        for j in top_cypher_bi:
+            couple.append((i, j))
+    for i in couple:
+        for j in couple:
+            if i != j or (j, i) not in pairs:
+                pairs.append((i, j))
+    return pairs
+
+
+def find_key(pair):
+    x1 = bi_to_number(pair[0][0])
+    x2 = bi_to_number(pair[0][1])
+    y1 = bi_to_number(pair[1][0])
+    y2 = bi_to_number(pair[1][1])
+    keys = []
+    a = linear_comp(x1 - x2, y1 - y2, m ** 2)
+    if a:
+        for i in a:
+            b = (gcd(y1 - i * x1, m ** 2)[1]) % m ** 2
+            keys.append((i, b))
+    return keys
+
+
+def all_keys(pairs):
+    keys = []
+    for pair in pairs:
+        key = find_key(pair)
+        if len(key) != 0:
+            for i in range(len(key)):
+                keys.append(key[i])
+    return keys
+
+
+pairs = pair(TEXT)
+
+keys = all_keys(pairs)
+
+texts = []
+
+for key in keys:
+    texts.append(decrypt(TEXT, key))
+
+def check(texts):
+    clean_text = []
+    for text in texts:
+        if not any(text.find("ьо"), text.find("оооо"), text.find("ьа")):
+            clean_text.append(text)
+    return clean_text
--- cp_3/zhurybeda_fb-91_cp_3/lab_math.py
+++ cp_3/zhurybeda_fb-91_cp_3/lab_math.py
@@ -3,16 +3,20 @@ def gcd(a, b):
         return 1, 0, a
     y, x, d = gcd(b, a % b)
     return x, y - (a // b) * x, d
-
-
+#a^-1, x, y
+# print(gcd(30, 50))
 # print(gcd(154, 803))
+
+def re(a,b):
+    x, _, _ = gcd(a, b)
+    return x
 
 
 def linear_comp(a, b, n):
     a_re, _, d = gcd(a, n)
     if d == 1:
         x = (a_re * b) % n
-        return x
+        return [x]
     else:
         solutions = []
         if b % d == 0:
@@ -20,9 +24,10 @@ def linear_comp(a, b, n):
             b1 = b / d
             n1 = n / d
             x = linear_comp(a1, b1, n1)
-            solutions.append(x)
+            solutions_loc = linear_comp(a1, b1, n1)
+            solutions.append(x[0])
             for i in range(1, d):
-                solutions.append(x + int(n / d) * i)
+                solutions.append((x[0] + int(n / d) * i) % n)
             return solutions
         else:
             return None
